12281d3e0d6ee6ec0c20bd76d058f2ac626f3148
Marko Manojlovic <marko@thecookieapps.com>
2016-04-13 18:14:26 +0000

    modified routes -> removed /pages/ from all_jobs URL

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'pages/index'
-  get 'pages/all_jobs'
+  # get 'pages/all_jobs'
+  get 'all_jobs' => 'pages#all_jobs'
 
   resources :jobs
   devise_for :users
